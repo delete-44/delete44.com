fd5c3a478e46ddf1cf9754e64eed1b312c83e426
Anthony <daviesanthonye1234@gmail.com>
2021-10-17 19:14:37 +0100

    Converted global styles to use sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import 'tachyons/css/tachyons.min.css'
-import '../styles/globals.css'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
