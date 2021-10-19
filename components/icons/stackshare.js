6a0a73bac456ff62ddccd7ee93e3fc2bb2147de5
delete44 <delete44+tech@protonmail.com>
2021-10-19 17:33:32 +0100

    Added padding to horizontal link

--- components/icons/stackshare.js
+++ components/icons/stackshare.js
@@ -4,7 +4,7 @@ export default function StackShare() {
   return (
     <a
       href={process.env.stackshareLink}
-      className="flex inline-flex items-center mh0-ns mh2"
+      className="flex inline-flex items-center mh0-ns mh2 pb1"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
