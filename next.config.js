34d208e5db705d67afeae3dcdfcc19cd5cdd877d
delete44 <delete44+tech@protonmail.com>
2021-10-20 16:59:54 +0100

    Config for using raw-loader to parse md and yml files

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  webpack: function (config) {
+    config.module.rules.push({ test: /\.md$/, use: "raw-loader" });
+    config.module.rules.push({ test: /\.yml$/, use: "raw-loader" });
+    return config;
   },
   env: {
     twitterLink: "https://twitter.com/_delete44",
     githubLink: "https://github.com/delete-44",
     linkedinLink: "https://www.linkedin.com/in/delete44/",
-    gravatarLink: "https://s.gravatar.com/avatar/27daea5cb28598ef30526f1586d9affb",
+    gravatarLink:
+      "https://s.gravatar.com/avatar/27daea5cb28598ef30526f1586d9affb",
     stackshareLink: "https://stackshare.io/delete-44/tools",
-  }
-}
+  },
+};
