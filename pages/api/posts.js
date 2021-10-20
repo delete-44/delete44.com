029b7f55bd6aa76f95c6ca993628959dc96f438e
delete44 <delete44+tech@protonmail.com>
2021-10-20 18:27:25 +0100

    Updated name of API route

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,5 +1,5 @@
 import matter from "gray-matter";
-import marked from 'marked'
+import marked from "marked";
 
 // Written by Pankaj Parashar, https://css-tricks.com/building-a-blog-with-next-js/
 
@@ -7,13 +7,13 @@ import marked from 'marked'
 // parse the front matter defined at the beginning of the
 // post using gray-matter and return the array of metadata
 // for all the posts.
-export async function getAllPosts() {
-  const context = require.context("../_posts", false, /\.md$/);
+export async function getPosts() {
+  const context = require.context("_posts", false);
   const posts = [];
 
   for (const key of context.keys()) {
     const post = key.slice(2);
-    const content = await import(`../_posts/${post}`);
+    const content = await import(`_posts/${post}`);
     const meta = matter(content.default);
 
     posts.push({
@@ -28,11 +28,11 @@ export async function getAllPosts() {
 // _posts directory, parse the Markdown with the marked library
 // and return the output HTML with metadata.
 export async function getPostBySlug(slug) {
-  const fileContent = await import(`../_posts/${slug}.md`)
-  const meta = matter(fileContent.default)
-  const content = marked(meta.content)
+  const fileContent = await import(`_posts/${slug}.md`);
+  const meta = matter(fileContent.default);
+  const content = marked(meta.content);
   return {
     title: meta.data.title,
-    content: content
-  }
+    content: content,
+  };
 }
