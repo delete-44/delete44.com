8dd124cdc988553e142b51f30d1ae4e540017a33
delete44 <delete44+tech@protonmail.com>
2021-10-23 16:02:16 +0100

    Added base icon layout

---
+++ layouts/icon_layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function IconLayout(props) {
+  return (
+    <a
+      href={props.link}
+      className={`${props.vertical ? "v" : "h"} flex-column inline-flex items-center mh0-ns mh2`}
+    >
+      {props.children}
+
+      <small className="tc pv0 mv0">{props.title}</small>
+    </a>
+  );
+}
