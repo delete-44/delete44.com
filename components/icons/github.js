9d01f4ba25669b71d3878b5d72635c621f71f4b5
delete44 <delete44+tech@protonmail.com>
2021-10-23 16:05:54 +0100

    Converted permanently-vertical links to use layout

--- components/icons/github.js
+++ components/icons/github.js
@@ -1,11 +1,9 @@
 import React from "react";
+import IconLayout from "@layouts/icon_layout";
 
 export default function GitHub() {
   return (
-    <a
-      href={process.env.githubLink}
-      className="flex-column inline-flex items-center mh0-ns mh2"
-    >
+    <IconLayout link={process.env.githubLink} title="GitHub" vertical>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="64"
@@ -20,8 +18,6 @@ export default function GitHub() {
         <title id="github_title">GitHub logo</title>
         <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
       </svg>
-
-      <small className="tc pv0 mv0">GitHub</small>
-    </a>
+    </IconLayout>
   );
 }
