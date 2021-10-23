aed8a41f4a2dd796176d160bcd14ffac8f368239
delete44 <delete44+tech@protonmail.com>
2021-10-23 16:08:26 +0100

    Expanded layout to respond to vertical option

--- layouts/icon_layout.js
+++ layouts/icon_layout.js
@@ -4,11 +4,15 @@ export default function IconLayout(props) {
   return (
     <a
       href={props.link}
-      className={`${props.vertical ? "v" : "h"} flex-column inline-flex items-center mh0-ns mh2`}
+      className={`${
+        props.vertical ? "flex-column" : "flex pb1"
+      } inline-flex items-center mh0-ns mh2`}
     >
       {props.children}
 
-      <small className="tc pv0 mv0">{props.title}</small>
+      <small className={`${props.vertical ? "tc" : "ml2"} pv0 mv0`}>
+        {props.title}
+      </small>
     </a>
   );
 }
