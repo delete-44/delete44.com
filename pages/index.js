f7fcc9a7658128b1d15dbcc97d3f007a8672f7b0
delete44 <delete44+tech@protonmail.com>
2021-10-23 16:47:57 +0100

    Updated footer layout

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import About from "@views/about";
 import Landing from "@views/landing";
 import TechnicalSkills from "@views/technical_skills";
 import Blog from "@views/blog";
+import GitHub from "@icons/github";
+import LinkedIn from "@icons/linkedin";
+import StackShare from "@icons/stackshare";
+import Twitter from "@icons/twitter";
+import WriteAs from "@icons/writeas";
 
 export default function Home() {
   return (
@@ -20,7 +25,17 @@ export default function Home() {
       <TechnicalSkills />
       <Blog />
 
-      <footer className="sc-child h4">&copy; Anthony | Delete44</footer>
+      <footer className="sc-child mt3 flex flex-column flex-row-ns justify-between items-center w-70-ns w-90 center">
+        <small>&copy; Anthony | Delete44</small>
+
+        <nav className="flex inline-flex-ns flex-wrap justify-center justify-between-ns w-40-ns w-90">
+          <GitHub />
+          <LinkedIn />
+          <Twitter />
+          <StackShare vertical />
+          <WriteAs vertical />
+        </nav>
+      </footer>
     </div>
   );
 }
