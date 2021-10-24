ceacf9a568737444a96ef149f9f36720f60dde1b
delete44 <delete44+tech@protonmail.com>
2021-10-24 15:36:05 +0100

    Manually wrote error page to avoid styling issues

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,17 @@
 import Head from "next/head";
-import Error from "next/error";
 
 export default function Custom404() {
   return (
     <div>
       <Head>
         <html lang="en-GB" />
+        <title>404: Page not found</title>
       </Head>
 
-      <Error statusCode={404} title="Page not found" />
+      <main className="flex items-center justify-center min-vh-100">
+        <h1 className="br b--secondary pr4">404</h1>
+        <p className="pl4">Page not found</p>
+      </main>
     </div>
   );
 }
