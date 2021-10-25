f6accbdb8a943ccac1c204fd8bb9789c2e001f5b
delete44 <delete44+tech@protonmail.com>
2021-10-25 11:27:53 +0100

    Used empty HTML wrapper on 404 page :exploding_head:

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 export default function Custom404() {
   return (
-    <div>
+    <>
       <Head>
         <html lang="en-GB" />
         <title>404: Page not found</title>
@@ -12,6 +12,6 @@ export default function Custom404() {
         <h1 className="br b--secondary pr4">404</h1>
         <p className="pl4">Page not found</p>
       </main>
-    </div>
+    </>
   );
 }
