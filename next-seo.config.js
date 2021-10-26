b333cf1da7d570437b6a65e7ac3b595dde2c6297
delete44 <delete44+tech@protonmail.com>
2021-10-26 09:40:00 +0100

    Added twitter config

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,5 +1,9 @@
 export default {
-  title: "delete44",
+  defaultTitle: "delete44",
   description: "Personal site of software developer delete44",
   canonical: "https://www.delete44.com",
+  twitter: {
+    cardType: "summary",
+    handle: "@_delete44",
+  },
 };
