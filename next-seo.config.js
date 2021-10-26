65c30d03c6128f5a8e11f61ec486469f8b710dd3
delete44 <delete44+tech@protonmail.com>
2021-10-26 09:43:31 +0100

    Added openGraph config

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,9 +1,23 @@
 export default {
-  defaultTitle: "delete44",
+  defaultTitle: "Delete44",
   description: "Personal site of software developer delete44",
   canonical: "https://www.delete44.com",
   twitter: {
     cardType: "summary",
     handle: "@_delete44",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://www.delete44.com",
+    title: "Delete44",
+    description: "Personal site of software developer delete44(*OG version)",
+    images: [
+      {
+        url: `${process.env.gravatarLink}?s=600`,
+        width: 600,
+        height: 600,
+        alt: "A picture of the site author, Anthony",
+      },
+    ],
   },
 };
