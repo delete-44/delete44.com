3becddb86feb6a8f9e898ea037557ff6be3cbac9
delete44 <delete44+tech@protonmail.com>
2021-10-26 10:06:34 +0100

    Moved all SEO into config file; expanded description

--- next-seo.config.js
+++ next-seo.config.js
@@ -10,14 +10,27 @@ export default {
     type: "website",
     url: "https://www.delete44.com",
     title: "Delete44",
-    description: "Personal site of software developer delete44(*OG version)",
+    description:
+      "Hi, and welcome to my site - I'm a software developer who sepcialises in Ruby on Rails, and am learning as much as I can about accessible design. Have a look...",
     images: [
       {
         url: `${process.env.gravatarLink}?s=600`,
         width: 600,
         height: 600,
-        alt: "A picture of the site author, Anthony",
+        alt: "A picture of the site author, delete44",
       },
     ],
   },
+  additionalLinkTags: [
+    {
+      rel: "icon",
+      href: `${process.env.gravatarLink}?s=16`,
+    },
+  ],
+  additionalMetaTags: [
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1"
+    }
+  ]
 };
